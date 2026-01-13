0cd400b15d9bef395b81a86097f195d367f4b55f
Rabia Noor <rabiasalman070@gmail.com>
2026-01-13 16:00:13 +0500

    Add files via upload

---
+++ 2nd program .cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,sum;
+	cout<<"Enter 1st number:";
+	cin>>a;
+	cout<<"Enter 2nd number:";
+	cin>>b;
+	sum=a+b;
+	cout<<"The sum of a and b is:"<<sum;
+	return 0;
+}
