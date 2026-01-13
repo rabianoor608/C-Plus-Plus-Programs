d16748d7fbdff3abf615760a8b3187a868562f9e
Rabia Noor <rabiasalman070@gmail.com>
2026-01-13 16:23:25 +0500

    Add files via upload

---
+++ IF statement program.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int value;
+	cout<<"Enter the value:";
+	cin>>value;
+	if(value<5)
+	cout<<"\n The value is less than 5 "<<endl;
+	return 0;
+}
